1f1fda1d87a104f615b4c10ae57fb71babe49529
rohitkothiya <rohit.kothiya2012@gmail.com>
2022-09-26 16:57:17 +0530

    Add page not found error

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import PostData from "../component/posts/post";
 import CommentData from "../component/comment/comment";
+import PageNotFound from "../component/pageNotfound";
 
 const RoutesCom = () => {
   return (
@@ -10,7 +11,7 @@ const RoutesCom = () => {
         <Route path="/posts" element={<PostData />} />
         <Route path="/comments" element={<CommentData />} />
         <Route path="/comments/postId/:id" element={<CommentData />} />
-        <Route path="*" element={<Navigate to="/posts" replace />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
