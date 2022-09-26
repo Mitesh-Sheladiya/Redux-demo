ed082afc9ac037397bf5eccd676490fa757d8e46
rohitkothiya <rohit.kothiya2012@gmail.com>
2022-09-26 16:22:34 +0530

    Route correction

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import PostData from "../component/posts/post";
 import CommentData from "../component/comment/comment";
 
@@ -6,10 +6,11 @@ const RoutesCom = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<PostData />} />
+        <Route path="/" element={<Navigate to="/posts" replace />} />
         <Route path="/posts" element={<PostData />} />
         <Route path="/comments" element={<CommentData />} />
         <Route path="/comments/postId/:id" element={<CommentData />} />
+        <Route path="*" element={<Navigate to="/posts" replace />} />
       </Routes>
     </>
   );
